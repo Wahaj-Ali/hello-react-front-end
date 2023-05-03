3af7203cb13bf3d5d72a1137fbe995efb6a5bcf8
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-03 21:20:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './Redux/configureStore';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HEllo</h1>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Greeting />
+      </Router>
+    </Provider>
   );
 }
 
